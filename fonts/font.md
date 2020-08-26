40f35560bdb96e4a96ccb17dd7d1679028ba2934
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2020-08-27 00:27:20 +0900

    v1.0.0

---
+++ fonts/font.md
@@ -0,0 +1,8 @@
+Mamelon(2020 Spring Release)
+https://moji-waku.com/mamelon/
+RondeB
+https://moji-waku.com/ronde/
+JKゴシックフォント
+http://font.cutegirl.jp/jk-font-light.html
+源真ゴシック
+http://jikasei.me/font/genshin/
---
+++ script.js
@@ -0,0 +1,35 @@
+var d = document;
+let text_input = d.getElementById("text_input");
+let test_wrap = d.getElementById("test_wrap");
+let text_show = d.getElementById("text_show");
+const mk_image = d.getElementById("bg_image");
+const output = d.getElementById("canvas_image");
+const capture = d.getElementById("capture");
+const sukasi_txt = d.getElementById("sukasi_txt");
+const canvas_draw = d.getElementById("canvas_draw");
+window.onload = panel_settings();
+function panel_settings() {
+  window.scrollTo(0,0);
+  const bg_1 = d.getElementById("bg_color_1").value;
+  const bg_2 = d.getElementById("bg_color_2").value;
+  const font = d.getElementById("font_color").value;
+  const fontType = d.getElementById('font_type').value;
+  const fontsize = d.getElementById("fontSize").value;
+  const text = text_input.value.replace(/\n/g, '<br>');
+  text_show.innerHTML = text;
+  capture.style.backgroundColor = bg_1;
+  test_wrap.style.backgroundColor = bg_2;
+  text_show.style.color = font;
+  text_show.style.fontSize = fontsize + "pt";
+  const xlines = d.getElementById( "XlineS" );
+  const xlineValue = xlines.xline.value;
+  const ylines = d.getElementById( "YlineS" );
+  const ylineValue = ylines.yline.value;
+  text_show.className = "";
+  text_show.classList.add(xlineValue);
+  text_show.classList.add(ylineValue);
+  html2canvas(capture).then(canvas => {
+    var imageData = canvas.toDataURL();
+    output.setAttribute("src",canvas.toDataURL());
+  });
+}
